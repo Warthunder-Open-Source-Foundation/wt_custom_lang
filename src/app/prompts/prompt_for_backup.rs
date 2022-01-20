90f4aa835d5365987d27743277368f33e4982010
FlareFlo <florianlipp17@gmail.com>
2022-01-20 20:43:29 +0100

    Fixed backup, needs naming function

--- src/app/prompts/prompt_for_backup.rs
+++ src/app/prompts/prompt_for_backup.rs
@@ -1,6 +1,7 @@
 use std::fs;
 use std::ops::Deref;
-use eframe::egui::{Button, CtxRef, Label, Window};
+use chrono::{NaiveDateTime, TimeZone};
+use eframe::egui::{Button, Color32, CtxRef, Label, RichText, Window};
 use fs_extra::dir::CopyOptions;
 use serde::{Deserialize, Serialize};
 use crate::{CustomLang, LANG_PATH};
@@ -31,8 +32,6 @@ fn create_backup(wt_folder: &str) {
 
 	let time = chrono::Local::now();
 	let path = format!("{}/{}/{}/backup_{}", &BACKUP_PATH.base_path, &BACKUP_PATH.config.project_prefix, &BACKUP_PATH.config.sub_folder, &time.timestamp().to_string());
-	println!("{}", &path);
-	println!("{}", wt_folder);
 
 	fs::create_dir_all(&path).unwrap();
 	fs_extra::dir::copy(wt_folder, &path, &COPY_OPTIONS).unwrap();
@@ -45,28 +44,44 @@ fn create_backup(wt_folder: &str) {
 	fs::write(&BACKUP_ENTRY_STORAGE(), bin).unwrap();
 }
 
+fn load_backup(wt_folder: &str, target_backup: &str) {
+	fs_extra::dir::copy(&target_backup,wt_folder,&COPY_OPTIONS).unwrap();
+}
+
 impl CustomLang {
 	pub fn prompt_for_backup(&mut self, ctx: &CtxRef) {
 		Window::new("Manage backups").show(ctx, |ui| {
-			ui.add(Label::new(fs::read_to_string(&BACKUP_ENTRY_STORAGE()).unwrap()));
+			ui.horizontal(|ui|{
+				if ui.add(Button::new("Create backup")).clicked() {
+					create_backup(&format!("{}/lang", &self.config.wt_path.as_ref().unwrap()));
+				}
 
-
-			if ui.add(Button::new("Create backup")).clicked() {
-				create_backup(&format!("{}/lang", &self.config.wt_path.as_ref().unwrap()));
-				self.prompt_for_backup = false;
-			}
-
-			if ui.add(Button::new("Close")).clicked() {
-				self.prompt_for_backup = false;
-			}
+				if ui.add(Button::new("Close")).clicked() {
+					self.prompt_for_backup = false;
+				}
+			});
 
 			ui.add_space(15.0);
 
+			let bin = fs::read_to_string(&BACKUP_ENTRY_STORAGE()).unwrap();
+			let mut backups: Vec<BackupEntry> = serde_json::from_str(&bin).unwrap();
 
-			// for backup in &backups {
-			// 	ui.add(Label::new(format!("{}", backup)));
-			// 	ui.add_space(5.0);
-			// }
+			for (i, backup) in backups.clone().iter().enumerate() {
+				ui.horizontal(|ui|{
+					let time = NaiveDateTime::from_timestamp(backup.date, 0).to_string();
+					ui.add(Label::new(format!("Created: {:?}", time)));
+					if ui.add(Button::new("Load")).clicked() {
+						load_backup(&format!("{}/lang", &self.config.wt_path.as_ref().unwrap()), &backup.dest);
+					}
+
+					if ui.add(Button::new(RichText::new("Delete backup").color(Color32::from_rgb(255, 0, 0)))).clicked() {
+						backups.remove(i);
+					}
+				});
+			}
+
+			let bin = serde_json::to_string(&backups).unwrap();
+			fs::write(&BACKUP_ENTRY_STORAGE(), bin).unwrap();
 		});
 	}
 }
