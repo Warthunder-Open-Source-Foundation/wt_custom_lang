ec7b8beb00ff0d69e339e76453f9e4b995ec1eba
flareflo <florianlipp17@gmail.com>
2022-01-21 08:12:21 +0100

    Removed unhandled unwraps from main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{fs};
 
 use eframe::{NativeOptions, run_native};
 use eframe::egui::Vec2;
+use notify_rust::Notification;
 
 use app::custom_lang::CustomLang;
 use crate::app::prompts::prompt_for_backup::BACKUP_ENTRY_STORAGE;
@@ -22,33 +23,58 @@ const CONFIG_NAME: &str = "wt_custom_lang"; //DO not change unless absolutely ne
 pub fn main() {
 	#[cfg(not(debug_assertions))]
 		std::panic::set_hook(Box::new(|panic_info| {
+		let err_notification = || {
+			// Error dropped as there is quite literally nothing that can be done at this point
+			let _ = Notification::new()
+				.summary("WT-custom-lang exited unexpectedly")
+				.body("if this issue keeps occurring please open an issue")
+				.show();
+		};
+
 		println!("{}", panic_info);
-		let dir = directories::BaseDirs::new().unwrap();
-		let data_dir = dir.data_dir().to_str().unwrap();
-		let final_path = &format!("{}/{}/error/{}.log", data_dir, CONFIG_NAME, chrono::offset::Local::now().format("%Y-%m-%d--%H-%M-%S"));
 
-		println!("{}", final_path);
+		if let Some(dir) = directories::BaseDirs::new() {
+			if let Some(data_dir) = dir.data_dir().to_str() {
+				let final_path = &format!("{}/{}/error/{}.log", data_dir, CONFIG_NAME, chrono::offset::Local::now().format("%Y-%m-%d--%H-%M-%S"));
 
-		fs::create_dir_all(&format!("{}/{}/error", data_dir, CONFIG_NAME));
+				let _ = fs::create_dir_all(&format!("{}/{}/error", data_dir, CONFIG_NAME));
 
-		match fs::write(final_path, panic_info.to_string()) {
-			Ok(_) => {
-				println!("Error log written to {}", final_path);
+				match fs::write(final_path, panic_info.to_string()) {
+					Ok(_) => {
+						println!("Error log written to {}", final_path);
+					}
+					Err(err) => {
+						println!("Failed to save error log due to:  {}", err);
+					}
+				}
+			} else {
+				err_notification();
 			}
-			Err(err) => {
-				println!("Failed to save error log due to:  {}", err);
-			}
+		} else {
+			err_notification();
 		}
 	}));
 
 	lazy_static::initialize(&LANG_PATH);
 
 	if fs::read(&LANG_PATH.constructed_path).is_err() {
-		fs::write(&LANG_PATH.constructed_path, b"[]").unwrap();
+		// "Save" unwrap as the panic handler will catch this, most likely will be a OS permission collision
+		match fs::write(&LANG_PATH.constructed_path, b"[]") {
+			Ok(_) => {}
+			Err(err) => {
+				panic!("{:?}", err);
+			}
+		};
 	}
 
 	if fs::read(&BACKUP_ENTRY_STORAGE()).is_err() {
-		fs::write(&BACKUP_ENTRY_STORAGE(), b"[]").unwrap();
+		// "Save" unwrap as the panic handler will catch this, most likely will be a OS permission collision
+		match fs::write(&BACKUP_ENTRY_STORAGE(), b"[]") {
+			Ok(_) => {}
+			Err(err) => {
+				panic!("{:?}", err);
+			}
+		};
 	}
 
 	let app = CustomLang::new();
